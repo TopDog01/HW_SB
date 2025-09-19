5e81aeabffc4a31034c07feed01f1412bbb38b49
Yuri Konev <crazy_clown93@mail.ru>
2025-09-19 19:31:14 +0300

    Anagramm

---
+++ Lesson_22/Anagram/Anagram.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+// Функция для проверки анаграмм
+
+bool areAnagrams(const string& str1, const string& str2) {
+    if (str1.length() != str2.length()) {
+        return false; // Если длины строк различаются, они не могут быть анаграммами
+    }
+
+    map<char, int> charCount;
+
+    // Подсчитываем символы в первой строке
+    for (char ch : str1) {
+        charCount[ch]++;
+    }
+
+    // Вычитаем символы из второй строки
+    for (char ch : str2) {
+        charCount[ch]--;
+        if (charCount[ch] < 0) {
+            return false; // Если количество символов становится отрицательным, строки не анаграммы
+        }
+    }
+
+    return true; // Если все символы в равном количестве, строки являются анаграммами
+}
+
+int main() {
+    string str1, str2;
+
+    cout << "Enter the first string: ";
+    getline(cin, str1);
+    cout << "Enter the second string: ";
+    getline(cin, str2);
+
+    if (areAnagrams(str1, str2)) {
+        cout << "The strings are anagrams." << endl;
+    }
+    else {
+        cout << "The strings are not anagrams." << endl;
+    }
+
+    return 0;
+}
