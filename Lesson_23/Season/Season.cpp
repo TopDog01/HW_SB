43e9e8029bdee9e024b69c23f33f175d459185ce
Yuri Konev <crazy_clown93@mail.ru>
2025-09-21 15:39:25 +0300

    season

---
+++ Lesson_23/Season/Season.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+// Определите один из макросов: SPRING, SUMMER, AUTUMN или WINTER
+#define SUMMER
+
+#if defined(SPRING)
+#define SEASON_NAME "Spring"
+#elif defined(SUMMER)
+#define SEASON_NAME "Summer"
+#elif defined(AUTUMN)
+#define SEASON_NAME "Autumn"
+#elif defined(WINTER)
+#define SEASON_NAME "Winter"
+#else
+#define SEASON_NAME "Unknown season"
+#endif
+
+int main() {
+
+
+    std::cout << "Current season: " << SEASON_NAME << std::endl;
+
+
+    return 0;
+}
